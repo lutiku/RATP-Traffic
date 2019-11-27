fed54d23739a18c2292c95549757fc451d7871f7
lutiku <lutiku.ndongala@edu.devinci.fr>
2019-11-27 15:07:24 +0100

    update files

--- js/script.js
+++ js/script.js
@@ -18,9 +18,12 @@ function getApiData()
         if(xhr.readyState===4)
         {
             const data = JSON.parse(xhr.responseText) ;
+
             const rers = data.result.rers.length
 
             const metros = data.result.metros.length
+
+            const tramways = data.result.tramways.length
             let hasProblem = false
 
 
@@ -43,9 +46,24 @@ function getApiData()
 
             }
 
+            for(let i = 0 ; i < tramways ; i++)
+            {
+                if(data.result.tramways[i].slug === 'critical')
+                {
+                    hasProblem = true
+                }
+
+            }
+
+
+
             document.querySelector('.status h1').innerHTML = hasProblem ? 'Incident(s)' : 'Trafic normal'
             templateType('rers', data.result.rers)
             templateType('metros', data.result.metros)
+            templateType('tramways', data.result.tramways)
+
+            showProblems(data.result)
+
 
         }
 
@@ -55,10 +73,43 @@ function getApiData()
     xhr.send()
 }
 
+function showProblems(data)
+{
+    const linesType =['rers', 'metros', 'tramways']
+
+    for (let i = 0; i< linesType.length; i++ )
+    {
+        const  lineType = linesType[i]
+
+        for(let j =0 ; j <data[lineType].length ; j++)
+        {
+            const line = data[lineType][j]
+
+            if(line.slug !=='normal')
+            {
+                templateProblem(lineType, line)
+            }
+        }
+    }
+}
+
+function templateProblem(lineType, line)
+{
+    let template = document.querySelector('.col--traffic')
+    template.innerHTML += '<div class="row">' +
+        '<div class="cols-sm-4">' + lineType + ' ' + line.line + '</div>' +
+        '<div class="col-sm-8">' + line.message + '</div>' +
+
+        '</di>'
+
+
+}
+
+
 function templateType(type, data)
     {
         let template = document.querySelector('.lines--' + type)
-        template.innerHTML = '<div class="col-sm-2">' + type + '</div>'
+        template.innerHTML += '<div class="col-sm-2">' + type + '</div>'
 
         const lines = data.length
 
@@ -69,6 +120,9 @@ function templateType(type, data)
 
         console.log(data)
     }
+
+
+
 function getBackgroundColor(slug)
 {
     switch (slug)
@@ -87,6 +141,10 @@ function getBackgroundColor(slug)
         break ;
     }
 }
+
+
+
+
 //Timer
 setInterval(function ()
     {
